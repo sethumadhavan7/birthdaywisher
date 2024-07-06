eb1f2c22dc09f3906324f777d371ee65d4cb902d
992200 <99220040728@klu.ac.in>
2024-07-06 19:05:10 +0530

    Update MongoDB connection string

--- birthday-wisher/server.js
+++ birthday-wisher/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+require('dotenv').config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/birthday-wisher', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -20,7 +22,7 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-app.post('/users', async (req, res) => {
+app.post('/api/users', async (req, res) => {
   const { name, relationship } = req.body;
   const user = new User({ name, relationship });
   await user.save();
@@ -46,6 +48,13 @@ app.post('/users', async (req, res) => {
   }
 
   res.status(201).send({ user, wish });
+});
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
